6f901d13a93c8d32922e434d984657d8d307f7ee
Rashed <rashedalamin69@gmail.com>
2025-12-01 00:10:10 +0600

    added express with typescript and created some of the crud function

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import express,{Express,Request,Response,NextFunction} from 'express'
+
+const app:Express=express()
+
+app.use(express.json())
+
+interface customReq extends Request{
+    startTime?:number
+}
+app.use((req:customReq,res:Response,next:NextFunction)=>{
+    req.startTime=Date.now()
+    next()
+})
+
+//create user    req {},{},{},{},{}  req.param,res, req.body, req.query, locals
+interface User{
+    name:string,
+    email:string
+}
+app.post('/user',(req:Request<{},{},User>,res:Response)=>{
+    const{name,email}=req.body
+    res.json({
+        message:`user ${name} is created ${email}`
+    })
+})
+
+//get a user from id
+app.get('/users/:id',(req:Request<{id:string}>,res:Response)=>{
+    const {id}=req.params
+    res.json({
+        userId:id
+    })
+})
+
+app.get("/",(req:Request,res:Response)=>{
+    res.send('hello typescript with express')
+})
+
+const port=3000;
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`)
+})
